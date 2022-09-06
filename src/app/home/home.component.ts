2688184936256e1aadb55aade03a90e332723e07
SuyogVyas <suyogvyas8@gmail.com>
2022-09-06 10:57:59 +0530

    new branch

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { CardModel } from '../model/cardmodel';
+import { ProductService } from '../Services/product/product.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _fb: FormBuilder, private _productService: ProductService) { }
+  productname: any = ''
+  practiceform!: FormGroup;
+  //subjectt$ = new Subject()
+  cardArray:any = []
+  card = new CardModel('Suyog','40000')
 
   ngOnInit(): void {
+
+    this.practiceform = this._fb.group({
+      productname: ['']
+    })
+
+    this.cardArray.push(this.card)
+    
+    // this.subjectt$.subscribe(data => {
+    //   this.card.name = data;
+    // })
+
+
   }
 
-}
+  submit(){
+    console.log(this.practiceform)
+
+    //this.subjectt$.next(this.practiceform.controls['productname'].value)
+    this.card = new CardModel(this.practiceform.controls['productname'].value,'400000')
+    this.cardArray.push(this.card)
+  }
+
+} 
